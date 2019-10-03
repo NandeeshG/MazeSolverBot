82b43e174e51ad14221b0e621bb5bf3865a16fa8
NandeeshG <nandeeshgupta@gmail.com>
2019-10-03 12:44:10 +0530

    Prog.ino and Motorcontrol.ino made

---
+++ prog.ino
@@ -0,0 +1,18 @@
+//Solve Maze Bot
+//TEAM - Aman Jain, Naman Arora, Namit Jain, Nandeesh Gupta
+
+const int lineFollowSensor0 = 12; //Using Digital input
+const int lineFollowSensor1 = 18; //Using Analog Pin A4 as Digital input
+const int lineFollowSensor2 = 17; //Using Analog Pin A3 as Digital input
+const int lineFollowSensor3 = 16; //Using Analog Pin A2 as Digital input
+const int lineFollowSensor4 = 19; //Using Analog Pin A5 as Digital input
+const int farRightSensorPin =  0; //Analog Pin A0
+const int farLeftSensorPin  =  1; //Analog Pin A1
+
+void setup() {
+}
+
+// the loop function runs over and over again forever
+void loop() {
+}
+
