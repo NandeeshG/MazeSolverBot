9b428625a4673ee40bf487eac6a80156f1c39704
NandeeshG <nandeeshgupta@gmail.com>
2019-10-03 13:07:14 +0530

    MotorControl complete

--- motorControlExample.ino
+++ motorControlExample.ino
@@ -1,85 +1,117 @@
 //EXAMPLE To control motor
+
 // connect motor controller pins to Arduino digital pins
 // motor one
-int enA = 10;
-int in1 = 9;
-int in2 = 8;
+int enA = 10;  //with PWM
+int inA1 = 9;
+int inA2 = 8;
 // motor two
-int enB = 5;
-int in3 = 7;
-int in4 = 6;
+int enB = 5;  //with PWM
+int inB1 = 7;
+int inB2 = 6;
+
 void setup()
 {
   // set all the motor control pins to outputs
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+  pinMode(inA1, OUTPUT);
+  pinMode(inA2, OUTPUT);
+  pinMode(inB1, OUTPUT);
+  pinMode(inB2, OUTPUT);
 }
-void demoOne()
+
+void motorA(int speed=0)
+{
+  // turn on motor A
+  if(speed>0)
+  {
+    digitalWrite(inA1, HIGH);
+    digitalWrite(inA2, LOW);
+  }
+  else if(speed<0)
+  {
+    digitalWrite(inA1, LOW);
+    digitalWrite(inA2, HIGH);
+  }
+  else
+  {
+    digitalWrite(inA1, LOW);
+    digitalWrite(inA2, LOW);
+  }
+  // set speed to speed out of possible range 0~255
+  analogWrite(enA, abs(speed)%256);
+}
+
+void motorB(int speed=0)
+{
+  if(speed>0)
+  {
+    digitalWrite(inB1, HIGH);
+    digitalWrite(inB2, LOW);
+  }
+  else if(speed<0)
+  {
+    digitalWrite(inB1, LOW);
+    digitalWrite(inB2, HIGH);
+  }
+  else
+  {
+    digitalWrite(inB1, LOW);
+    digitalWrite(inB2, LOW);
+  }
+  analogWrite(enB, abs(speed)%256);
+}
+
+void move1()
 {
   // this function will run the motors in both directions at a fixed speed
-  // turn on motor A
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  // set speed to 200 out of possible range 0~255
-  analogWrite(enA, 200);
-  // turn on motor B
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  // set speed to 200 out of possible range 0~255
-  analogWrite(enB, 200);
+  motorA(200);
+  motorB(200);
   delay(2000);
+
   // now change motor directions
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);  
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH); 
+  motorA(-200);
+  motorB(-200);
   delay(2000);
+
   // now turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);  
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
+  motorA();
+  motorB();
 }
-void demoTwo()
+
+void move2()
 {
   // this function will run the motors across the range of possible speeds
   // note that maximum speed is determined by the motor itself and the operating voltage
   // the PWM values sent by analogWrite() are fractions of the maximum speed possible 
   // by your hardware
   // turn on motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);  
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH); 
+
   // accelerate from zero to maximum speed
-  for (int i = 0; i < 256; i++)
+  for(int i=0; i<256; ++i)
   {
-    analogWrite(enA, i);
-    analogWrite(enB, i);
+    motorA(i);
+    motorB(i);
     delay(20);
-  } 
+  }
   // decelerate from maximum speed to zero
-  for (int i = 255; i >= 0; --i)
+  for(int i=255; i>=0; --i)
   {
-    analogWrite(enA, i);
-    analogWrite(enB, i);
+    motorA(i);
+    motorB(i);
     delay(20);
-  } 
+  }
   // now turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);  
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);  
+  motorA();
+  motorB();
 }
+
 void loop()
 {
-  demoOne();
+  move1();
   delay(1000);
-  demoTwo();
+  move2();
   delay(1000);
 }
 
